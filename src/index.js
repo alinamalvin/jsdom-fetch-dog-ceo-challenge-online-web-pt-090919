5ae19160c2d4c0c55c075228ab0acf32d92953b9
Alina Kornilova <alinakornilova78@gmail.com>
2020-04-02 00:48:17 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
 console.log('%c HI', 'color: firebrick')
-document.addEventListener("DOMContentLoaded", function(){
-  const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
-  const dogImageContainer = document.getElementById("dog-image-container");
-  fetch(imgUrl)
-  .then(resp => resp.json())
-  .then(json => console.log(json))
-  dogImageContainer.appendChild(imgUrl)
 
-  const breedUrl = 'https://dog.ceo/api/breeds/list/all'
-  const dogBreeds = document.getElementById("dog-breeds")
-  const li = document.createElement('li')
-  fetch(breedUrl)
-  .then(resp => resp.json())
-  .then(json => console.log(json))
-  dogBreeds.appendChild(li)
-  li.appendChild(breedUrl)
+function getImages(){
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
 
-  li.addEventListener('click', (ev) {
-    ev.target.color = "yellow";
-  })
-})
+    return fetch(imgUrl)
+    .then(resp => resp.json())
+    .then(json => renderImages(json));
+}
+
+function renderImages(json){
+    let imgBox = document.querySelector("div#dog-image-container");
+    json.message.forEach(image_url => {
+        imgTag = document.createElement("img");
+        imgTag.setAttribute("src", `${image_url}`);
+        imgBox.appendChild(imgTag);
+    });
+}
+
+document.addEventListener("DOMContentLoaded", (e) => {
+    getImages();
+});
