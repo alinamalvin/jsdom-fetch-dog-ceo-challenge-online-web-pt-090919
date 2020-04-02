6d3dee9b7ce4387cb4c39287a95a5e8a689bdbc1
Alina Kornilova <alinakornilova78@gmail.com>
2020-04-02 00:32:10 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ console.log('%c HI', 'color: firebrick')
 
 fetchImage('https://dog.ceo/api/breeds/image/random/4')
   .then(resp => resp.json())
-  .then(json => renderBooks(json));
+  .then(json => console.log(json));
 
